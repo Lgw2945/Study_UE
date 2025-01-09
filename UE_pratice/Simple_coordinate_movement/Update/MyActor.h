edbd5cc564b9caf4bdab59a27227973f1614d434
Lee_gw <89513932+Lgw2945@users.noreply.github.com>
2025-01-09 12:28:05 +0900

    Create MyActor.h

---
+++ UE_pratice/Simple_coordinate_movement/Update/MyActor.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class CH33_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void move();
+
+	int32 step();
+
+	float distance(FVector2D first, FVector2D second);
+
+	void TriggerEventWithProbability(float Probability);
+
+private:
+	FVector2D start;
+	float totDist;
+	int32 sum;
+};
