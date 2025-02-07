4461c401b37aa019a1224cb22b6b6e3b5fb30de8
Lee_gw <89513932+Lgw2945@users.noreply.github.com>
2025-02-07 13:10:02 +0900

    Delete UE_pratice/Pawn_Character/MyGameMode.cpp

--- UE_pratice/Pawn_Character/MyGameMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyGameMode.h"
-#include "MyCharacter.h"
-#include "MyPlayerController.h"
-
-AMyGameMode::AMyGameMode()
-{
-	DefaultPawnClass = AMyCharacter::StaticClass(); // 클래스 이름으로 호출::객체 생성을 하지 않고 선언, 반환 (스태틱)
-	PlayerControllerClass = AMyPlayerController::StaticClass();
-}
